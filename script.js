d3c9b4deb6e450bbe11a5dba4d7a27eaa9cb166f
ShiyaraSanthosh <shiyarasanthosh3@gmail.com>
2025-06-25 23:15:56 +0530

    todo

--- script.js
+++ script.js
@@ -2,15 +2,52 @@ const inputbox = document.getElementById("input_box");
 const adding = document.getElementById("add");
 const taskbox = document.getElementById("task_box");
 //step 2
-function displayinput(){
-    let inputdata = inputbox.value;
-    //create DOM element
-    let li = document.createElement('li');
-    li.innerText = inputdata;
-    taskbox.appendChild(li);
-    inputbox.value='';
-    console.log(li);
+function addTask(task){
+    const todolist = document.getElementById("task_box");
+    const li = document.createElement("li");
+    li.innerHTML = ` <li class="task">
+              <label>
+                <input type="checkbox" />
+                <span class="task-text">${task}</span>
+              </label>
+              <div class="edit_btn">
+                <button class="edit">Edit</button>
+                <button class="delete">Delete</button>
+              </div>
+            </li>
+        `;
+        todolist.appendChild(li)
 }
-//step 1
-//add element in inputbox
-adding.addEventListener("click",displayinput);
+ //step 1
+ //form submission
+ document.getElementById("todo_form") . addEventListener("submit",function(event){
+    event.preventDefault();
+    const inputTask = document.getElementById("input_box");
+    const task = inputTask.value.trim();
+    if(task !== ''){
+        addTask(task);
+        inputTask.value ="";
+        inputTask.focus();  
+
+    }
+
+
+ });
+
+ //delete button
+ document.getElementById("task_box") . addEventListener("click",function(event){
+    if(event.target.classList.contains("delete")){
+        event.target.parentElement.parentElement.remove();
+    }
+ });
+
+ //edit button 
+ document.getElementById("task_box") . addEventListener("click",function(event){
+    if(event.target.classList.contains("edit")){
+        const taskText  = event.target.parentElement.parentElement.querySelector("span");
+        const newText  = prompt("enter a new text",taskText.textContent);
+        if (newText !== null) {
+            taskText.textContent = newText.trim();
+        }
+    }
+ });
